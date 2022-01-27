a85f71bad32cc0bf0d50954c9b37e0541b619c24
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-01-27 01:27:43 -0300

    Objetos. Dando vida a nuestras clases en Java y Python.

--- CursoPOOUber/Java/Car.java
+++ CursoPOOUber/Java/Car.java
@@ -3,4 +3,12 @@ public class Car {
     String license;
     String driver;
     Integer passegenger;
+
+    void printDataCar() {
+        System.out.println("Id: " + id);
+        System.out.println("Car " + id + " License: " + license);
+        System.out.println("Car " + id + " Driver: " + driver);
+        System.out.println("Car " + id + " Passegenger: " + passegenger);
+    }
 }
+
