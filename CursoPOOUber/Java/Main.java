95138d2b6d33f5b05fddb81ec5af05972375f797
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-01-27 04:10:27 -0300

    Declarando un Método Constructor en Java y JavaScript.

--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,18 +1,14 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo.");
-        Car car = new Car();
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "ADN123"));
         car.id = 1;
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
         car.passegenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car.id = 2;
-        car2.license = "QWE567";
-        car2.driver = "Fernando Gonzalez";
-        car2.passegenger = 3;
-        car.printDataCar();
+        // Car car2 = new Car("AMQ123", new Account("Andres Herrera", "ADN123"));
+        // car.id = 2;
+        // car2.passegenger = 3;
+        // car.printDataCar();
     }
 }
