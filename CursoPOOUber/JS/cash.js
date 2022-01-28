fd8e1ec0677731c64d5da40929552f04bcdf575f
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-01-27 22:51:07 -0300

    Reto 4

--- CursoPOOUber/JS/cash.js
+++
@@ -1,6 +0,0 @@
-class Cash extends Payment {
-    constructor (id, amount) {
-        super(id);
-        this.amount = amount;
-    }
-}
--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -6,4 +6,10 @@ car.passegenger = 4;
 
 let carX = new UberX('JPA567', conductor, 'Testa', 'S');
 carX.passegenger = 4;
-carX.printDataCar();
+// carX.printDataCar();
+
+let user1 = new User(123, "Juan Perez", "JP123456");
+user1.printUserData();
+
+let driver1 = new Driver(789, "Pedro Lopez", "PL987654");
+driver1.printUserData();
